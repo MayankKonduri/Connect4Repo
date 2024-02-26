c1e50dbc63a76e8c4880c13b39603e73349b16e5
MayankKonduri <mayank.konduri@gmail.com>
2024-02-26 12:12:56 -0600

    Second_Update(2/26/24)

--- src/ConnectFourCommandFromClient.java
+++ src/ConnectFourCommandFromClient.java
@@ -1,2 +1,10 @@
-public class ConnectFourCommandFromClient {
+import java.io.Serializable;
+public class ConnectFourCommandFromClient implements Serializable {
+    private int column;
+    public ConnectFourCommandFromClient(int column){
+        this.column = column;
+    }
+    public int getColumn(){
+        return column;
+    }
 }
--- src/ConnectFourCommandFromServer.java
+++ src/ConnectFourCommandFromServer.java
@@ -1,2 +1,12 @@
-public class ConnectFourCommandFromServer {
+import java.io.Serializable;
+public class ConnectFourCommandFromServer implements Serializable{
+    private char[][] board;
+
+    public ConnectFourCommandFromServer(char[][] board){
+        this.board = board;
+    }
+
+    public char[][] getBoard(){
+        return board;
+    }
 }
